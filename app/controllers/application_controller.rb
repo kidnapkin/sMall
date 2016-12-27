3c24ce3db8a26afdfd8edb94d2f7d727cabe0e0b
Kid Napkin <e.domotenko@gmail.com>
2016-12-27 14:29:49 +0000

    nested categories views :sparkles:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # include CurrentCart
-  # before_action :set_cart
+  include CurrentCart
+  before_action :set_cart
   protect_from_forgery with: :exception
 end
