681ec0ebe2dc4bc446591e729062a1b180613c7b
Kid Napkin <e.domotenko@gmail.com>
2017-01-11 21:47:41 +0000

    add stars

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,20 @@
 module ReviewsHelper
+    
+  def show_stars(product)
+    average_rating = product.reviews.published.any? ? product.reviews.published.average(:rating) * 20 : 0
+
+    raw("<div class='star-ratings-sprite'>" +
+        "<span style='width: #{average_rating.to_i}%' class='star-ratings-sprite-rating'>" +
+        "</span>" +
+        "</div>")
+  end
+  
+  def show_review_stars(review)
+    review_rating = review.published ? review.rating * 20 : 0
+
+    raw("<div class='star-ratings-sprite'>" +
+        "<span style='width: #{review_rating.to_i}%' class='star-ratings-sprite-rating'>" +
+        "</span>" +
+        "</div>")
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id           :integer          not null, primary key
+#  review       :text
+#  product_id   :integer
+#  author_name  :string
+#  author_email :string
+#  published    :boolean          default(FALSE)
+#  rating       :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Review < ApplicationRecord
+  validates :review, :product_id, :author_name, :author_email, :rating, presence: true
   belongs_to :product
+  
+  scope :published, -> {  where(published: true) }
 end
